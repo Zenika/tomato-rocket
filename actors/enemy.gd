efec368f0eeb6bd4ee36b4418d70eb4ff3cad2d7
AudreyGentili <audrey.gentili@zenika.com>
2024-02-10 16:02:47 +0100

    feat: ajout astéroides apparaissant aléatoirement

--- actors/enemy.gd
+++ actors/enemy.gd
@@ -1,25 +1,31 @@
 extends Area2D
 
-var vertical_speed = 0
+var vertical_speed
 var speed_coef
+const side_movement = 0.4
+
+var velocity = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	rotation = randf() * PI
+	var scale_ratio = randf_range(0.5, 4)
+	scale = Vector2(scale_ratio, scale_ratio)
+
 	speed_coef = randf()
+	
+	velocity.x += randf_range(-side_movement, side_movement)
+	velocity.y += 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized()
+	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO
-	velocity.y += 1
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized()
-		
 	position += delta * velocity * vertical_speed
-	print("position ",position)	
 	
 func set_vertical_speed(speed):
-	print(speed_coef)
 	vertical_speed = speed * speed_coef
 
 func _on_collision_polygon_2d_body_entered(body):
--- scenes/race_screen.gd
+++ scenes/race_screen.gd
@@ -1,28 +1,29 @@
 extends Node2D
 
+@export var enemy_scene: PackedScene
 @export var finish_line_scene: PackedScene
 @export var vertical_speed = 200
 const speed_multiplicator = 50
 var is_finished = false
 
-const race_length = 10000
+const race_length = 100000
 const window_height = 950
+const window_width = 1920
 var traveled_distance = 0
 
 func new_game():
+	$EnemyTimer.start()
 	$Player.start($PlayerPosition.position)
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	new_game()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if (is_finished):
 		vertical_speed = 0
 	else:
 		vertical_speed = vertical_speed + delta * speed_multiplicator
-	$Enemy.set_vertical_speed(vertical_speed)
+	# $Enemy.set_vertical_speed(vertical_speed)
 	$Background.set_vertical_speed(vertical_speed)
 	traveled_distance += vertical_speed * delta
 	if (traveled_distance > race_length - window_height):
@@ -33,3 +34,10 @@ func finish_game():
 	is_finished = true
 	print(traveled_distance)
 	$Player.stop($PlayerPosition.position)
+
+
+func _on_enemy_timer_timeout():
+	var enemy = enemy_scene.instantiate()
+	enemy.position = Vector2(window_width * randf(), 0)
+	add_child(enemy)
+	enemy.set_vertical_speed(vertical_speed)
