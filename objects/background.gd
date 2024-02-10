1997878454dc6cacd99208a9528dbe3f2114abe2
Nirina Rabeson <nirina.rabeson@zenika.com>
2024-02-10 22:07:05 +0100

    feat: l'arrière plan se déplace à la vitesse du vaisseau

--- objects/background.gd
+++ objects/background.gd
@@ -4,7 +4,7 @@ var vertical_speed
 var c = 2
  
 func _process(delta: float) -> void:
-	var camera_velocity = Vector2(0, min(vertical_speed, 800) * c)
+	var camera_velocity = Vector2(0, vertical_speed * c)
 	var new_offset: Vector2 = get_scroll_offset() + camera_velocity * delta
 	set_scroll_offset(new_offset)
 	
