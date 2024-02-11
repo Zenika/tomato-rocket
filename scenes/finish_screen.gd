1b1300fd18e9198e0ae646a3d6e138dd72aa9c09
Will Munn <william.munn@rubix.com>
2024-02-11 17:55:58 +0100

    Game over sound

--- scenes/finish_screen.gd
+++ scenes/finish_screen.gd
@@ -1,9 +1,10 @@
 extends Node2D
+@onready var audio_stream_player_2d = $AudioStreamPlayer2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	audio_stream_player_2d.play() # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
